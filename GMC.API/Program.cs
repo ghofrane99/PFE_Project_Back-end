97a612accea886463514164e3650bce982242f27
ghofrane99 <ghofrane.chaouachi@esprit.tn>
2023-03-21 17:09:12 +0100

    AddPackage

--- GMC.API/Program.cs
+++ GMC.API/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddCors((corsoptions) =>
         policyoptions.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
     });
 });
+
+
 builder.Services.AddScoped<IPickListRepository, PickListRepository>();
 builder.Services.AddScoped<IPickListService, PickListService>();
 builder.Services.AddScoped<ILigneProductionRepository, LigneProductionRepository>();
@@ -37,6 +39,7 @@ builder.Services.AddScoped<IRemoteUSRepository, RemoteUSRepository>();
 builder.Services.AddScoped<IRemoteUSService, RemoteUSService>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
